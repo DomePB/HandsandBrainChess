92cff7fc5f81c0e8fcec13d480a5d09f1624226a
Dominik Radomski <dominik.radomski@hhu.de>
2023-03-23 20:02:35 +0100

    Send moves trough console

--- src/main/java/com/example/handandbrainchess/WebController.java
+++ src/main/java/com/example/handandbrainchess/WebController.java
@@ -1,12 +1,18 @@
 package com.example.handandbrainchess;
 
 import com.example.handandbrainchess.Service.ApplicationService;
+import com.example.handandbrainchess.domain.Feld;
+import com.example.handandbrainchess.domain.Figuren;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class WebController {
@@ -16,17 +22,20 @@ public class WebController {
     private Integer moveRow2;
     private Integer moveColumn1;
     private Integer moveColumn2;
+
+    private Figuren[][] Feld;
     public WebController(ApplicationService service) {
         this.service = service;
     }
 
     @GetMapping("/")
     public String index(Model m){
-        m.addAttribute("Feld",service.getFeld());
+        Feld = service.getFeld();
+        m.addAttribute("Feld",Feld);
         return "start";
     }
     @GetMapping("/move")
-    @SendTo("/endpoint/topic")
+    @SendTo("/topic/greetings")
     public String move(Model m, @RequestParam("idrow") int idrow, @RequestParam("idcolumn") int idcolumn){
       //  System.out.println("Reihe: " + idrow + " Spalte: "+ idcolumn);
       //  m.addAttribute("Feld",service.getFeld());
@@ -43,10 +52,6 @@ public class WebController {
         }
         if(moveRow2 != null && moveColumn2 != null){
             service.move(moveRow1,moveColumn1,moveRow2,moveColumn2);
-            moveRow1 = null;
-            moveRow2 = null;
-            moveColumn1 = null;
-            moveColumn2= null;
         }
         return "redirect:/";
     }
@@ -55,5 +60,32 @@ public class WebController {
     public String init(){
         service.init();
         return "redirect:/";
+    }
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public List<Integer> greet(){
+        //Feld = service.getFeld();
+        //return Feld;
+        List<Integer> list = new ArrayList<>();
+        if(moveRow1 != null){
+            list.add(moveRow1);
+        }
+        if(moveColumn1 != null){
+            list.add(moveColumn1);
+        }
+        if(moveRow2 != null){
+            list.add(moveRow2);
+        }
+        if(moveColumn2 != null){
+            list.add(moveColumn2);
+            moveRow1 = null;
+            moveRow2 = null;
+            moveColumn1 = null;
+            moveColumn2= null;
+        }
+        if(list.size()>0){
+            return list;
+        }
+        return null;
     }
 }
